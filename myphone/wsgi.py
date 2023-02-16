d7804b725f7982f460d92accc973d36773c7f1f5
Omarsanosy24 <113290018+Omarsanosy24@users.noreply.github.com>
2023-02-16 13:05:41 +0200

    Update wsgi.py

--- myphone/wsgi.py
+++ myphone/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myphone.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myphone.settings.base")
 
 application = get_wsgi_application()
